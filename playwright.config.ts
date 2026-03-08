44bd58d7eb3ca704340e4a348bd10f1a60f52f09
ENOSISBD\mayeen <mayeen.sajid@enosisbd.com>
2026-03-08 14:13:51 +0600

    Add Playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  retries: 0,
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }]
+  ],
+});
